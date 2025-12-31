a1601ee23ba0f1a915fa62d39e1ba55ab8378487
visal2002 <115523620+visal2002@users.noreply.github.com>
2025-12-31 10:47:57 +0600

    Update base path in Vite configuration to match GitHub repository name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/vishal_chettri_portfolio/',
+  base: '/Portfolio/', // Must match your GitHub repo name
   plugins: [react()],
   server: {
     port: 3000
